6334b0ba4e6d01b706cff91f6cd720eceb71f1ae
hun1001 <l01022899266@gmail.com>
2022-10-17 23:46:45 +0900

    스텟은 모르겠고 베네핏이 급하기 때문에 프로토타입용 코드만 적어놓고 모험을 베네핏으로 떠나겠습니다

---
+++ Assets/01. Scripts/CreateCanvas/TrainTextController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrainTextController : MonoBehaviour
+{
+    [SerializeField]
+    private Text tailText = null;
+
+    [SerializeField]
+    private Text middleText = null;
+
+    [SerializeField]
+    private Text headText = null;
+
+    public void SetTailText(string text)
+    {
+        tailText.text = text;
+    }
+
+    public void SetMiddleText(string text)
+    {
+        middleText.text = text;
+    }
+
+    public void SetHeadText(string text)
+    {
+        headText.text = text;
+    }
+}
