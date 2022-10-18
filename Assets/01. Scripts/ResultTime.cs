56517829033968751a3d64920cc92ba4c400a74e
hun1001 <l01022899266@gmail.com>
2022-10-19 00:21:29 +0900

    치트로 엔딩까지! 그러나 카메라 흔들리는 부작용이?

--- Assets/01. Scripts/ResultTime.cs
+++ Assets/01. Scripts/ResultTime.cs
@@ -1,18 +1,34 @@
+using System.Reflection.Emit;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ResultTime : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public float time = 20.0f;
 
+    private void Update()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            time = 0;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnGUI()
     {
+        GUIStyle style = new GUIStyle();
+        style.fontSize = 20;
+        style.normal.textColor = Color.black;
+        GUI.Label(new Rect(10, 10, 100, 20), time.ToString("F2") + " seconds left to the destination", style);
 
+        if (time == 0)
+        {
+            style.normal.textColor = Color.red;
+            GUI.Label(new Rect(50, 100, 100, 50), "You have arrived at the destination", style);
+            Application.Quit();
+        }
     }
+
 }
