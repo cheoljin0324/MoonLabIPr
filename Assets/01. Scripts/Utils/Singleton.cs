cbcc95bead9189a6a115b0ea6e768030ddbf65f6
hun1001 <l01022899266@gmail.com>
2022-10-25 00:40:35 +0900

    이번 생은 정리하며 살아보기로 했습니다!

--- Assets/01. Scripts/Utils/Singleton.cs
+++ Assets/01. Scripts/Utils/Singleton.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
     private static T instance;
     public static T Instance
