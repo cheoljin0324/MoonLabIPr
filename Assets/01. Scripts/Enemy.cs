8f392ff46a72a32bd62dfb8947e53e0778766f4f
hun1001 <l01022899266@gmail.com>
2022-11-04 20:20:42 +0900

    왕의 명령대로 수행하였으나 결과가 좋지 않았기에 추방당했습니다

--- Assets/01. Scripts/Enemy.cs
+++ Assets/01. Scripts/Enemy.cs
@@ -12,6 +12,7 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         StartCoroutine(Attack());
+        StartCoroutine(Move());
     }
 
     private IEnumerator Attack()
@@ -26,6 +27,24 @@ public class Enemy : MonoBehaviour
             yield return new WaitForSeconds(Random.Range(atkSpeed, atkSpeed + 0.5f));
             GameObject bulletObj = Instantiate(bullet, transform.position, Quaternion.identity);
             bulletObj.GetComponent<Bullet>()._parent = gameObject;
+        }
+    }
+
+
+
+    private IEnumerator Move()
+    {
+        Vector3 dir = new Vector3(1, 0, 0);
+        while (true)
+        {
+            if (!canMove)
+            {
+                yield return null;
+                continue;
+            }
+            yield return new WaitForSeconds(Random.Range(0.5f, 1.0f));
+            dir *= -1;
+            transform.Translate(dir * 0.1f);
         }
     }
 }
