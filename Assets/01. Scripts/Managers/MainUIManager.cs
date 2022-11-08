46dd1663c0936a946df7871099aada99d381ea14
seomin1210 <seoyun0700@daum.net>
2022-11-08 23:27:51 +0900

    UI Animation Edit

--- Assets/01. Scripts/Managers/MainUIManager.cs
+++ Assets/01. Scripts/Managers/MainUIManager.cs
@@ -6,9 +6,13 @@ using DG.Tweening;
 
 public class MainUIManager : Singleton<MainUIManager>
 {
+    private Canvas openCanvas;
+
     private Vector3 startScale = Vector3.zero;
     private Vector3 originScale;
-    private Canvas openCanvas;
+
+    private Vector3 startPos = new Vector3(0f, 2f, 0f);
+    private float originYPos = 1f;
 
     public Text nameText;
     public UserNameSO _userNameSO;
@@ -18,16 +22,26 @@ public class MainUIManager : Singleton<MainUIManager>
         nameText.text = _userNameSO.name;
     }
 
-    public void OpenUI(GameObject ui)
+    public void OpenBiggerUI(GameObject ui)
     {
         openCanvas = ui.GetComponent<Canvas>();
         if (openCanvas != null)
         {
-            openCanvas.renderMode = RenderMode.WorldSpace;
             originScale = ui.transform.localScale;
             ui.transform.localScale = startScale;
             openCanvas.enabled = true;
-            ui.transform.DOScale(originScale, 0.5f);
+            ui.transform.DOScale(originScale, 0.2f);
+        }
+    }
+
+    public void OpenDownUI(GameObject ui)
+    {
+        openCanvas = ui.GetComponent<Canvas>();
+        if (openCanvas != null)
+        {
+            ui.transform.position += startPos;
+            openCanvas.enabled = true;
+            ui.transform.DOMoveY(originYPos, 0.3f);
         }
     }
 }
