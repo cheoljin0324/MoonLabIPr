4df89f2f01d86daf6e3d77c696ba0db1ac53d170
hun1001 <l01022899266@gmail.com>
2022-11-08 22:00:17 +0900

    신의 실수로 죽어 개사기 모델을 가지고 전생하였습니다

--- Assets/01. Scripts/MapMove.cs
+++ Assets/01. Scripts/MapMove.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 
 public class MapMove : MonoBehaviour
 {
-    private MeshRenderer _meshRenderer;
-
-    private float offset = 0.0f;
-
     public float speed = 0.0f;
-
-    private void Start()
-    {
-        _meshRenderer = GetComponent<MeshRenderer>();
-    }
 
     private void Update()
     {
-        offset += Time.deltaTime * speed;
-        _meshRenderer.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
     }
 }
