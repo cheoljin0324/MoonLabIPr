2bf28b2734ebbe040cf55aafd7407527f04eae4d
seomin1210 <seoyun0700@daum.net>
2022-11-13 12:55:52 +0900

    Gacha System framework add

---
+++ Assets/01. Scripts/GachaSystem.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GachaSystem : MonoBehaviour
+{
+    public enum RARITY
+    {
+        RARE = 100,
+        SR = 30,
+        SSR = 5,
+    }
+
+    private float randomRarity = 0f;
+
+    private GameObject[] rareCharacter;
+
+    public void Gacha()
+    {
+        randomRarity = Random.Range(0f, 100f);
+        if(randomRarity <= (float)RARITY.SSR)
+        {
+            SSRGacha();
+        }
+        else if(randomRarity <= (float)RARITY.SR)
+        {
+            SRGacha();
+        }
+        else if(randomRarity <= (float)RARITY.RARE)
+        {
+            RareGacha();
+        }
+    }
+
+    public void RareGacha()
+    {
+        randomRarity = Random.Range(0, rareCharacter.Length);
+
+        //return rareCharacter[(int)randomRarity];
+    }
+
+    public void SRGacha()
+    {
+
+    }
+
+    public void SSRGacha()
+    {
+
+    }
+}
