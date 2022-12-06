32736a99ed2609d398b90303bacba7fbded08771
hun1001 <l01022899266@gmail.com>
2022-12-06 15:48:36 +0900

    3d 모델 추가 정 타겟팅하는 스크립트 추가

---
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform target;
+    
+    void Update()
+    {
+        Vector2 direction = new Vector2(target.position.x - transform.position.x, target.position.z - transform.position.z);
+        direction.Normalize();
+        transform.rotation = Quaternion.Euler(-90f, Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + 90f, 0f);
+    }
+}
