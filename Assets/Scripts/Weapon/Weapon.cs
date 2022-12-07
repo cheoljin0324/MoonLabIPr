ebdc938808ce5c204a698bc7d02b143120f11ecb
hun1001 <l01022899266@gmail.com>
2022-12-07 14:06:30 +0900

    아웃라인 추가

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -10,6 +10,7 @@ public class Weapon : MonoBehaviour
     {
         Vector2 direction = new Vector2(target.position.x - transform.position.x, target.position.z - transform.position.z);
         direction.Normalize();
-        transform.rotation = Quaternion.Euler(-90f, Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + 90f, 0f);
+        
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(-90f, Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + 90f, 0f), 0.1f);
     }
 }
