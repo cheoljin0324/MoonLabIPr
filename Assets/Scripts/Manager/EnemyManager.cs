6858dee038d63cc776bdbabf8b44763866708ec3
seomin1210 <seoyun0700@daum.net>
2022-12-11 00:06:19 +0900

    Monster Spawn

--- Assets/Scripts/Manager/EnemyManager.cs
+++ Assets/Scripts/Manager/EnemyManager.cs
@@ -10,12 +10,34 @@ public class EnemyManager : MonoSingleton<EnemyManager>
     protected override void Awake()
     {
         base.Awake();
-        CreatePool();
+        Invoke("CreatePool", 0.5f);
     }
-    
+
+    private void Start()
+    {
+        StartCoroutine(EnemySpawn());
+    }
+
     private void CreatePool()
     {
         foreach (PoolingPair pair in _initList.list)
             PoolManager.Instance.CreatePool(pair.prefab, pair.poolCnt);
+    }
+
+    IEnumerator EnemySpawn()
+    {
+        Enemy enemy;
+        yield return new WaitForSeconds(3f);
+        while (true)
+        {
+            enemy = PoolManager.Instance.Pop("EnemyTest") as Enemy;
+
+            float randomPosZ = Random.Range(-20f, 0f);
+            Vector3 pos = CombatManager.Instance.Train.transform.position;
+            pos.x -= 7f;
+            pos.z += randomPosZ;
+
+            enemy.transform.position = pos;
+        }
     }
 }
