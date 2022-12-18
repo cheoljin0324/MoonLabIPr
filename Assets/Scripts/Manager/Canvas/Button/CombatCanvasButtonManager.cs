1e4d472ed5c6efdd2419e803ee8be3bd0e467ee2
seomin1210 <seoyun0700@daum.net>
2022-12-18 13:19:05 +0900

    전투 UI 추가

---
+++ Assets/Scripts/Manager/Canvas/Button/CombatCanvasButtonManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatCanvasButtonManager : ButtonManager
+{
+    public void OnMenuButtonClicked()
+    {
+        // 메뉴 Canvas 나오게해야함
+        // 게임 진행상황 일시중지
+    }
+}
