172382943dda6966e1e08c6ce3dc532a02ea45ca
seomin1210 <seoyun0700@daum.net>
2022-12-21 00:15:28 +0900

    피격시 폭발 이펙트 나타남

---
+++ Assets/Fx Explosion Pack/Script/DestroyEffect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyEffect : PoolableMono
+{
+    public override void Reset()
+    {
+        // 이펙트나오는 시간으로 수정?
+        Invoke("Push", 2f);
+    }
+
+    private void Push()
+    {
+        PoolManager.Instance.Push(this);
+    }
+}
