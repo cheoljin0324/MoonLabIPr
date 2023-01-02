6eed57074e2893594a95a94513f58e62ef705518
seomin1210 <seoyun0700@daum.net>
2023-01-02 19:44:36 +0900

    메뉴 ui 수정

--- Assets/Scripts/Manager/Canvas/Button/CombatCanvasButtonManager.cs
+++ Assets/Scripts/Manager/Canvas/Button/CombatCanvasButtonManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CombatCanvasButtonManager : ButtonManager
 {
+    private float _xPos;
+
     public void OnMenuButtonClicked()
     {
         // 메뉴 Canvas 나오게해야함
@@ -13,5 +15,41 @@ public class CombatCanvasButtonManager : ButtonManager
     public void OnSkill1ButtonClicked()
     {
         SkillManager.Instance.ThunderSkill();
+    }
+
+    public void OnFirstCameraButtonClicked()
+    {
+        _xPos = CombatManager.Instance.Train.TrainCars[0].gameObject.transform.position.x;
+        Camera.main.GetComponent<CameraController>();
+    }
+
+    public void OnSecondCameraButtonClicked()
+    {
+        _xPos = CombatManager.Instance.Train.TrainCars[1].gameObject.transform.position.x;
+        Camera.main.transform.Translate(new Vector3(_xPos, 0f));
+    }
+
+    public void OnThirdCameraButtonClicked()
+    {
+        _xPos = CombatManager.Instance.Train.TrainCars[2].gameObject.transform.position.x;
+        Camera.main.transform.Translate(new Vector3(_xPos, 0f));
+    }
+
+    public void OnFourthCameraButtonClicked()
+    {
+        _xPos = CombatManager.Instance.Train.TrainCars[3].gameObject.transform.position.x;
+        Camera.main.transform.Translate(new Vector3(_xPos, 0f));
+    }
+
+    public void OnFifthCameraButtonClicked()
+    {
+        _xPos = CombatManager.Instance.Train.TrainCars[4].gameObject.transform.position.x;
+        Camera.main.transform.Translate(new Vector3(_xPos, 0f));
+    }
+
+    public void OnSixthCameraButtonClicked()
+    {
+        _xPos = CombatManager.Instance.Train.TrainCars[5].gameObject.transform.position.x;
+        Camera.main.transform.Translate(new Vector3(_xPos, 0f));
     }
 }
